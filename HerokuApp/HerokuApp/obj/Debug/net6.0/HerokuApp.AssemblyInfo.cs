6dfbd7662f5a8f7616bb56601be77b74be698ad4
Anna Samchenko <anna_ginz@list.ru>
2023-06-28 10:21:58 +0500

    fix

--- HerokuApp/HerokuApp/obj/Debug/net6.0/HerokuApp.AssemblyInfo.cs
+++ HerokuApp/HerokuApp/obj/Debug/net6.0/HerokuApp.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("SauceDemo")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("HerokuApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("SauceDemo")]
-[assembly: System.Reflection.AssemblyTitleAttribute("SauceDemo")]
+[assembly: System.Reflection.AssemblyProductAttribute("HerokuApp")]
+[assembly: System.Reflection.AssemblyTitleAttribute("HerokuApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
